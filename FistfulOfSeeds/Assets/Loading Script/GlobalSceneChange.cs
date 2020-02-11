64c19f4b07a8e6bc04d7153284b1cc5789050fe3
AJVer <ajverburg@hotmail.com>
2020-02-11 10:37:37 -0800

    Added conditional loading between scenes

---
+++ FistfulOfSeeds/Assets/Loading Script/GlobalSceneChange.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlobalSceneChange : MonoBehaviour
+{
+    public static string sceneFrom;
+    public static string locationFrom;
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ FistfulOfSeeds/Assets/Loading Script/LoadingScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+    public string sceneToLoad;
+    public string locationName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player" )
+        {
+            GlobalSceneChange.locationFrom = locationName;
+            GlobalSceneChange.sceneFrom = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(sceneToLoad);
+        }
+    }
+}
