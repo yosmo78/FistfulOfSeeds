bf1624aba5c66e67892f8641f77379b4cadf3895
werdo1756 <loganmikulski@gmail.com>
2020-02-11 10:52:55 -0800

    player death and camera

--- FistfulOfSeeds/Assets/Logan Files/Logan Scripts/PlayerAttack.cs
+++ FistfulOfSeeds/Assets/Logan Files/Logan Scripts/PlayerAttack.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -12,10 +13,17 @@ public class PlayerAttack : MonoBehaviour
     public Animator animator;
     public float attackRange;
     public int damage;
+    public int health = 3;
+    private int healthRem = 0;
+    public float invinciTime = 2;
+    private float invinciTimeRem = 0;
+    public bool invinciBool = false;
 
     // Start is called before the first frame update
     void Start()
     {
+        healthRem = health;
+        invinciTimeRem = invinciTime;
         
     }
 
@@ -42,11 +50,48 @@ public class PlayerAttack : MonoBehaviour
         {
             timeBtwAttack -= Time.deltaTime;
         }
+
+        if (invinciBool == true)
+        {
+            invinciTime -= Time.deltaTime;
+
+            if (invinciTime <= 0)
+            {
+                invinciTime = invinciTimeRem;
+                invinciBool = false;
+            }
+        }
     }
 
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    void Hurt()
+    {
+        if(invinciBool == false)
+        {
+            health--;
+            invinciBool = true;
+
+            if (health <= 0)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                health = healthRem;
+                invinciBool = false;
+            }
+        }
+        
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        enemy1Path enemy = collision.collider.GetComponent<enemy1Path>();
+
+        if (enemy != null)
+        {
+            Hurt();
+        }
     }
 }
