27a30d535deefa6606e581e24c5fda32ccccd132
yosmo78 <yosmo78@gmail.com>
2020-02-21 19:01:39 -0800

    Started to dynamically create plots and plants

--- FistfulOfSeeds/Assets/Scripts/GMScript.cs
+++ FistfulOfSeeds/Assets/Scripts/GMScript.cs
@@ -2,15 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class GMScript : MonoBehaviour
 {
 
 	public Transform grassObj;
+    public Transform plotObj;
 	public static string currentTool = "none";
+
+    public static int numberOfPlotsWidth = 2;
+    public static int numberOfPlotsHeight = 2; 
+
+    public static plotTile[,] farm;
 
 	public static int sunFlowerSeeds = 5;
 	public static int potatoSeeds = 4;
 	public static int carrotSeeds = 3;
+    //public static
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +30,18 @@ public class GMScript : MonoBehaviour
     		{
     			Instantiate (grassObj, new Vector2 (xPos, yPos), grassObj.rotation);
     		}
-    	}   
+    	} 
+        //Instantate plot
+        farm = new plotTile[numberOfPlotsWidth, numberOfPlotsHeight];
+
+        for(int i  =  0; i < numberOfPlotsWidth; ++i)
+        {
+            for(int j = 0 ; j < numberOfPlotsHeight; ++j)
+            {
+                farm[i, j].plot = Instantiate(plotObj,new Vector2(i,j), new Quaternion(0,0,0,0));  
+            }
+        }
+
     }
 
     // Update is called once per frame
