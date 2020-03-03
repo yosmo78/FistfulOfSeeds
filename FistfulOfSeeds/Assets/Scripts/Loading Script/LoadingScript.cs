373ddafd4fba002acb11145f2989a9f854c1ddc6
AJVer <ajverburg@hotmail.com>
2020-03-03 10:16:52 -0800

    Added main menu and pause menu

---
+++ FistfulOfSeeds/Assets/Scripts/Loading Script/LoadingScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScript : MonoBehaviour
+{
+    public string loadingName;
+    public string sceneTo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.name == "Player" )
+        {
+            if (sceneTo.Equals("Farm"))
+            {
+                FindObjectOfType<CameraFollow>().isFollowing = false;
+                collision.attachedRigidbody.bodyType = RigidbodyType2D.Static;
+            }
+            GlobalSceneChange.locationFrom = loadingName;
+            GlobalSceneChange.sceneFrom = SceneManager.GetActiveScene().name;
+            SceneManager.LoadScene(sceneTo);
+        }
+    }
+}
---
+++ FistfulOfSeeds/Assets/Scripts/Loading Script/PlayerStart.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStart : MonoBehaviour
+{
+
+    private logan_player_controller player;
+    public string sceneFrom;
+    public string pointNameFrom;
+    public Vector3 cameraStart; //only used if not following
+    // Start is called before the first frame update
+
+    void Start()
+    {
+        
+        if( GlobalSceneChange.sceneFrom.Equals(sceneFrom) &&
+            GlobalSceneChange.locationFrom.Equals(pointNameFrom))
+        {
+            Debug.Log(GlobalSceneChange.sceneFrom);
+            Debug.Log(GlobalSceneChange.locationFrom);
+            player = FindObjectOfType<logan_player_controller>();
+            Debug.Log(player);
+            player.transform.position = transform.position;
+
+            if(!FindObjectOfType<CameraFollow>().isFollowing)
+            {
+                FindObjectOfType<Camera>().transform.position = cameraStart;  
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
