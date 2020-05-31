f50fd1914bd482800458a516a9aeef35466ea502
Tiger Chiang <chiang,tiger@yahoo.com>
2020-05-30 20:03:37 -0700

    They drop seeds but you can't pick it up because thats hard

---
+++ FistfulOfSeeds/Assets/Scripts/Pickup.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DapperDino.Items;
+using DapperDino.Items.Inventories;
+
+public class Pickup : MonoBehaviour
+{
+    /*
+    //public GameObject inventory;
+    [SerializeField] public Inventory inventory;
+    [SerializeField] ItemSlot testItemSlot = new ItemSlot();
+    private void OnTriggerEnter2D(Collider2D other) {
+        testItemSlot.AddItem(testItemSlot);
+    
+    }
+    */
+}
